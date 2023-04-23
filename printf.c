32b55aeb87888819546cbf479de1aacbfc8ae43c
iWiafe <ismailawiafe@yahoo.com>
2023-04-23 11:49:48 -0700

    Education is when you read the fine print. Experience is what you get if you don't

--- printf.c
+++ printf.c
@@ -4,19 +4,6 @@
 #include <unistd.h>
 
 /**
- * print_buffer - prints the contents of the buffer if it exist
- * @buffer: Array of chars
- * @buff_ind: Index at which to add next char, represents the length.
- */
-void print_buffer(char buffer[], int *buff_ind)
-{
-	if (*buff_ind > 0)
-		write(1, &buffer[0], *buff_ind);
-
-	*buff_ind = 0;
-}
-
-/**
  * _printf - a function that produces output according to a format
  * @format: format.
  * Return: number of chars printed.
@@ -62,3 +49,17 @@ int _printf(const char *format, ...)
 	va_end(list);
 	return (printed_chars);
 }
+
+/**
+ * print_buffer - Prints the contents of the buffer if it exist
+ * @buffer: Array of chars
+ * @buff_ind: Index at which to add next char, represents the length.
+ */
+void print_buffer(char buffer[], int *buff_ind)
+{
+	if (*buff_ind > 0)
+		write(1, &buffer[0], *buff_ind);
+
+	*buff_ind = 0;
+}
+
