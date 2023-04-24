afd5f97840d56890861fa2c06c975d71e036fe38
SIPHIWE1995 <siphiwencane21@gmail.com>
2023-04-24 08:35:09 -0700

    team work

--- size.c
+++ size.c
@@ -1,61 +1,27 @@
-#include "main.h"
-
-/**
- * _size - Calculates the size to cast the argument
- * @format: Formatted string in which to print the arguments
- * @i: List of arguments to be printed.
- *
- * Return: Precision.
- */
-int _size(const char *format, int *i)
-{
-	int curr_i = *i + 1;
-	int size = 0;
-
-	if (format[curr_i] == 'l')
-		size = S_LONG;
-	else if (format[curr_i] == 'h')
-		size = S_SHORT;
-
-	if (size == 0)
-		*i = curr_i - 1;
-	else
-		*i = curr_i;
-
-	return (size);
+#include <string.h>
+#include <stdio.h>
+#include <stdarg.h>
+char* rot13(char* str) {
+    char* output = str;
+    for (int i = 0; i < strlen(str); i++) {
+        if (isalpha(str[i])) {
+            if ((str[i] >= 'a' && str[i] <= 'm') || (str[i] >= 'A' && str[i] <= 'M')) {
+                output[i] = str[i] + 13;
+            } else {
+                output[i] = str[i] - 13;
+            }
+        }
+    }
+    return output;
 }
 
-/**
- * _width - Calculates the width for printing
- * @format: Formatted string in which to print the arguments.
- * @i: List of arguments to be printed.
- * @list: list of arguments.
- *
- * Return: width.
- */
-int _width(const char *format, int *i, va_list list)
-{
-	int curr_i;
-	int width = 0;
-
-	for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
-	{
-		if (is_digit(format[curr_i]))
-		{
-			width *= 10;
-			width += format[curr_i] - '0';
-		}
-		else if (format[curr_i] == '*')
-		{
-			curr_i++;
-			width = va_arg(list, int);
-			break;
-		}
-		else
-			break;
-	}
-
-	*i = curr_i - 1;
-
-	return (width);
+void print_R(char* output, const char* arg) {
+    char* encoded = rot13(arg);
+    sprintf(output, "%s", encoded);
+}
+int main() {
+    const char* message = "The flood of print has turned reading into a process of gulping rather than savoring";
+    printf("Original message: %s\n", message);
+    printf("Rot13 encoded message: %R\n", message);
+    return 0;
 }
