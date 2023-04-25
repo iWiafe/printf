6ba67cfc12f171f634775d720cd0b7a078d96580
SIPHIWE1995 <siphiwencane21@gmail.com>
2023-04-25 12:45:21 -0700

    printf

--- size.c
+++ size.c
@@ -1,30 +1,26 @@
-#ifndef MAIN_H
-#define MAIN_H
-#include <stdarg.h>
-#include <stdio.h>
-#include <unistd.h>
+#include "main.h"
 
-char* rot13(char* str) {
-    char* output = str;
-    for (int i = 0; i < strlen(str); i++) {
-        if (isalpha(str[i])) {
-            if ((str[i] >= 'a' && str[i] <= 'm') || (str[i] >= 'A' && str[i] <= 'M')) {
-                output[i] = str[i] + 13;
-            } else {
-                output[i] = str[i] - 13;
-            }
-        }
-    }
-    return output;
-}
+/**
+ * get_size - Calculates the size to cast the argument
+ * @format: Formatted string in which to print the arguments
+ * @i: List of arguments to be printed.
+ *
+ * Return: Precision.
+ */
+int get_size(const char *format, int *i)
+{
+	int curr_i = *i + 1;
+	int size = 0;
 
-void print_R(char* output, const char* arg) {
-    char* encoded = rot13(arg);
-    sprintf(output, "%s", encoded);
-}
-int main() {
-    const char* message = "The flood of print has turned reading into a process of gulping rather than savoring";
-    printf("Original message: %s\n", message);
-    printf("Rot13 encoded message: %R\n", message);
-    return 0;
+	if (format[curr_i] == 'l')
+		size = S_LONG;
+	else if (format[curr_i] == 'h')
+		size = S_SHORT;
+
+	if (size == 0)
+		*i = curr_i - 1;
+	else
+		*i = curr_i;
+
+	return (size);
 }
