b9eb76fc476b46e721591906b9fcb8313a685f51
iWiafe <ismailawiafe@yahoo.com>
2023-04-26 16:24:31 -0700

    printf

--- printf.c
+++ printf.c
@@ -1,11 +1,22 @@
 #include "main.h"
 
-void print_buffer(char buffer[], int *buff_ind);
+/**
+ * print_buffer - Prints the contents of the buffer if it exist
+ * @buffer: Array of chars
+ * @buff_ind: Index at which to add next char, represents the length.
+ */
+void print_buffer(char buffer[], int *buff_ind)
+{
+	if (*buff_ind > 0)
+		write(1, &buffer[0], *buff_ind);
+
+	*buff_ind = 0;
+}
 
 /**
- * _printf - Printf function
+ * _printf - a function that produces output according to a format
  * @format: format.
- * Return: Printed chars.
+ * Return: number of chars printed.
  */
 int _printf(const char *format, ...)
 {
@@ -44,24 +55,7 @@ int _printf(const char *format, ...)
 			printed_chars += printed;
 		}
 	}
-
 	print_buffer(buffer, &buff_ind);
-
 	va_end(list);
-
 	return (printed_chars);
 }
-
-/**
- * print_buffer - Prints the contents of the buffer if it exist
- * @buffer: Array of chars
- * @buff_ind: Index at which to add next char, represents the length.
- */
-void print_buffer(char buffer[], int *buff_ind)
-{
-	if (*buff_ind > 0)
-		write(1, &buffer[0], *buff_ind);
-
-	*buff_ind = 0;
-}
-
